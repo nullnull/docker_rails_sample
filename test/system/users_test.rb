1d524f9ec079e62ca8462eccdff1af5da05d2b17
Katsuma Narisawa <face.the.music721@gmail.com>
2017-10-24 00:17:29 +0900

    Revert "Set invalid username to ensure testing on circle ci"
    This reverts commit 4e085d93460c59448623b2b5025999e3993fc6a5.

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -1,9 +1,9 @@
-require 'application_system_test_case'
+require "application_system_test_case"
 
 class UsersTest < ApplicationSystemTestCase
-  test 'visiting the index' do
+  test "visiting the index" do
     visit users_url
 
-    assert_selector 'h1', text: 'The User'
+    assert_selector "h1", text: "User"
   end
 end
